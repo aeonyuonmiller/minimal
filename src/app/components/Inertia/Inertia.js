2030fff30ffd1654e493f74191228b98d3bce928
Aeon Yuon Miller <aym1@mail.com>
2025-08-22 01:52:09 +0200

    smoothened

--- src/app/components/Inertia/Inertia.js
+++ src/app/components/Inertia/Inertia.js
@@ -54,9 +54,9 @@ export default function Inertia({title}) {
         ease: "power4.out",
         scrollTrigger: {
           trigger: root.querySelector(".medias"),
-          start: "top 60%", // when top of container hits 80% of viewport
+          start: "top bottom", // when top of container hits 80% of viewport
           end: "bottom top",
-          toggleActions: "play reverse restart restart",
+          toggleActions: "play reverse restart reverse",
         },
       }
     );
